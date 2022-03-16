09f30871ba4a3e3f49568ba3b63f27df9278a31f
Anil <meghanilva@live.in>
2022-03-16 16:38:34 +0530

    checkout start

--- src/test/java/org/selenium/pom/objects/BillingAdress.java
+++ src/test/java/org/selenium/pom/objects/BillingAdress.java
@@ -1,5 +1,53 @@
 package org.selenium.pom.objects;
 
 public class BillingAdress {
+private String firstName, lastName,addressLineOne,city,postalCode,email;
 
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getAddressLineOne() {
+	return addressLineOne;
+}
+
+public void setAddressLineOne(String addressLineOne) {
+	this.addressLineOne = addressLineOne;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+public String getPostalCode() {
+	return postalCode;
+}
+
+public void setPostalCode(String postalCode) {
+	this.postalCode = postalCode;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
 }
--- src/test/java/org/selenium/pom/pages/CheckoutPage.java
+++ src/test/java/org/selenium/pom/pages/CheckoutPage.java
@@ -8,4 +8,35 @@ public class CheckoutPage extends BasePage{
 CheckoutPage(WebDriver driver){
 	super(driver);
 }
+public CheckoutPage enterFirstName() {
+	driver.findElement(By.id("billing_first_name")).clear();
+driver.findElement(By.id("billing_first_name")).sendKeys("anil");
+return this;
+}
+public CheckoutPage enterLastName() {
+	driver.findElement(By.id("billing_last_name")).clear();
+driver.findElement(By.id("billing_last_name")).sendKeys("anil");
+return this;
+}
+public CheckoutPage enterAddressLineOne() {
+	driver.findElement(By.id("billing_address_1")).clear();
+driver.findElement(By.id("billing_address_1")).sendKeys("anil");
+return this;
+}
+public CheckoutPage enterCity() {
+	driver.findElement(By.id("billing_city")).clear();
+driver.findElement(By.id("billing_city")).sendKeys("anil");
+return this;
+}
+public CheckoutPage enterPostalcode() {
+	driver.findElement(By.id("billing_postcode")).clear();
+driver.findElement(By.id("billing_postcode")).sendKeys("anil");
+return this;
+}
+public CheckoutPage enterEmail() {
+	driver.findElement(By.id("billing_email")).clear();
+driver.findElement(By.id("billing_email")).sendKeys("anil");
+return this;
+}
+
 }
